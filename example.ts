e415051e2e88d710ca0c16be2c9e1246f53dc726
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-06-12 20:26:56 -0500

    :memo: Add readme

---
+++ example.ts
@@ -0,0 +1,9 @@
+import { roman } from "./mod.ts";
+
+console.log(roman(3)); //=> "III"
+
+console.log(roman(47)); //=> "XLVII"
+
+console.log(roman(3990)); //=> "MMMCMXC"
+
+console.log(roman(1984)); //=> "MCMLXXXIV"
