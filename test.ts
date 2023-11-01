eebfebc424e726b600407be7c20217398a50b089
GitHub <noreply@github.com>
2023-11-01 00:16:22 +0000

    ⬆️  Update dependencies

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import { parseRoman, roman } from "./mod.ts";
 import {
   assertEquals,
   assertThrows,
-} from "https://deno.land/std@0.175.0/testing/asserts.ts";
+} from "https://deno.land/std@0.204.0/testing/asserts.ts";
 
 const cases = [
   [1, "I"],
